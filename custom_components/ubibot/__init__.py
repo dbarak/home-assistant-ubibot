e0c9ade98a0fec1ea5c4a2ad8a045dd37a6fb703
dbarak <danie@barak1.com>
2025-05-02 08:50:30 -0400

    NEW - Update __init__.py

--- custom_components/ubibot/__init__.py
+++ custom_components/ubibot/__init__.py
@@ -1,18 +1,1 @@
-"""UbiBot integration."""
-import voluptuous as vol
-from homeassistant.const import CONF_SCAN_INTERVAL
-import homeassistant.helpers.config_validation as cv
-from .const import CONF_ACCOUNT_KEY, CONF_CHANNEL_ID, DOMAIN
-
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {
-                vol.Required(CONF_ACCOUNT_KEY): cv.string,
-                vol.Required(CONF_CHANNEL_ID): cv.string,
-                vol.Optional(CONF_SCAN_INTERVAL, default=900): cv.positive_int,
-            }
-        )
-    },
-    extra=vol.ALLOW_EXTRA,
-)
+"""Ubibot custom integration.  Just a sensor platform, no setup needed."""
