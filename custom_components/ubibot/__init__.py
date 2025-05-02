441fa99eee7930ecd4e1954dedf827b96703a8da
dbarak <danie@barak1.com>
2025-05-02 08:34:55 -0400

    Update __init__.py

--- custom_components/ubibot/__init__.py
+++ custom_components/ubibot/__init__.py
@@ -1,23 +1,18 @@
-"""Ubibot component."""
-
-import logging
-
+"""UbiBot integration."""
 import voluptuous as vol
-
-from homeassistant.const import CONF_API_KEY, CONF_SCAN_INTERVAL
-from homeassistant.helpers import config_validation as cv
-
-_LOGGER = logging.getLogger(__name__)
-
-
-CONF_CHANNEL = "channel"
-
-DOMAIN = "ubibot"
+from homeassistant.const import CONF_SCAN_INTERVAL
+import homeassistant.helpers.config_validation as cv
+from .const import CONF_ACCOUNT_KEY, CONF_CHANNEL_ID, DOMAIN
 
 CONFIG_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_API_KEY): cv.string,
-        vol.Required(CONF_CHANNEL): cv.string,
-        vol.Required(CONF_SCAN_INTERVAL, default=900): cv.positive_int,
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_ACCOUNT_KEY): cv.string,
+                vol.Required(CONF_CHANNEL_ID): cv.string,
+                vol.Optional(CONF_SCAN_INTERVAL, default=900): cv.positive_int,
+            }
+        )
     },
+    extra=vol.ALLOW_EXTRA,
 )
